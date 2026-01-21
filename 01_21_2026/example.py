90130071206be16f024786e9ff1550567604c30f
Addison Boyer <AddisonB@consumerdirectcare.com>
2026-01-21 11:25:40 -0700

    Adding Python files from Today's lecture.

---
+++ 01_21_2026/example.py
@@ -0,0 +1,16 @@
+import sys
+import stdio
+
+a = 1234
+b = 99
+c = a + b
+
+# Literal Python data-type value
+
+# Operator.
+
+# Identifier - can't be a keyword.
+mystring = "hello"
+
+# Variables.
+# Constant variable.
---
+++ 01_21_2026/useargument.py
@@ -0,0 +1,6 @@
+import sys
+import stdio
+
+stdio.write("Hello, ")
+stdio.write("World")
+stdio.write(sys.argv[2])
